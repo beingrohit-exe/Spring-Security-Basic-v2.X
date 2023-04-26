28202d6800b9bead393528bbc884393c0c7c50e2
Rohit Parihar <rohitparih@gmail.com>
2023-04-26 21:35:03 +0530

    Logging and Password Encoder

--- src/main/java/com/security/security_basics/Configuration/WebSecurity.java
+++ src/main/java/com/security/security_basics/Configuration/WebSecurity.java
@@ -1,9 +1,16 @@
 package com.security.security_basics.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author - rohit
@@ -19,10 +26,31 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf(
+                )
+                .disable()
                 .authorizeRequests()
-                .anyRequest()
+                .antMatchers(HttpMethod.POST)
+                .permitAll()
+                .antMatchers(HttpMethod.GET)
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("rohit").password(passwordEncoder().encode("rohit")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("hello").password(passwordEncoder().encode("hello")).roles("USER");
+    }
+
+    /*@Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 }
