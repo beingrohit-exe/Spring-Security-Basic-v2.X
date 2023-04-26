6afb827612ba66818e8bb6c398ac809a28ddd3fb
Rohit Parihar <rohitparih@gmail.com>
2023-04-26 14:12:15 +0530

    Initialized

---
+++ src/test/java/com/security/security_basics/SecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.security_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
